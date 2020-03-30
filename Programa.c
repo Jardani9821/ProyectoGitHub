d1458637ecbdfc6578c6d4cdd08b730629e4bd8f
root <root@localhost.localdomain>
2020-03-30 07:36:33 +0000

    Modificacion por uzziel

--- Programa.c
+++ Programa.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 main(){
        printf("Hola mundo este es mi primer commit\n");
+       printf("Hola mundo modificacion 2 por uzziel");
+       printf("Hola mundo modificacion 3 por uzziel");
        
 }
 
